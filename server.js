67a7bb4f7dd43dbea0c4f534297457c7dbbeec19
sunnychauhan-dev <sunnychauhan1924@gmail.com>
2026-01-29 14:45:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
-//cofig env
 dotenv.config();
-
-//database config
 connectDB();
 
 const app = express();
@@ -17,26 +14,36 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   "https://ecommerce-project-frontend-p8ki.onrender.com",
-  "http://localhost:5173", // for local dev
+  "http://localhost:5173",
 ];
 
 // middlewares
 app.use(
   cors({
-    origin: allowedOrigins,
-    credentials: true,
-  }),
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
+
 app.use(express.json());
 
-//routes
+// routes
 app.use("/api/auth", authRoute);
 app.use("/api/category", categoryRoutes);
-app.use("/api/product", productRoutes)
+app.use("/api/product", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the new project</h1>");
 });
+
 app.listen(PORT, () => {
   console.log(`Server started at port: ${PORT}`);
 });
+
